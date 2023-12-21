8d442399c2c42c3e77fe8e6679638317915f3929
Genielson <ge.nielson@hotmail.com>
2023-12-21 20:22:22 -0300

    feat: Creating a initial User model

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,12 +53,3 @@ $route['default_controller'] = '';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-/* API */
-$route['product'] = 'api/Product';
-$route['product/(:any)'] = 'api/Product/$1';
-$route['product/(:num)']['PUT'] = 'api/Product/$1';
-$route['product/(:num)']['DELETE'] = 'api/Product/$1';
-$route['register'] = 'api/User/register';
-$route['login'] = 'api/User/login';
-$route['logout'] = 'api/User/logout';
-$route['reGenToken'] = 'api/Token/reGenToken';
