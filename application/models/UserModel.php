56fd3ddaab8f8cbbe7d1848ee96a6313ed1a9fca
Genielson <ge.nielson@hotmail.com>
2023-12-21 20:27:38 -0300

    feat: Implementation of the resolveUserLogin method in UserModel

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -22,17 +22,23 @@ class UserModel extends CI_Model {
 	}
 
 	public function createUser($username, $email, $password) {
-		
 		$data = array(
 			'username'   => $username,
 			'email'      => $email,
 			'password'   => $this->hash_password($password),
 			'created_at' => date('Y-m-j H:i:s'),
 		);
-		
 		$this->db->insert('users', $data);
 		return $this->db->insert_id(); 
 		
+	}
+
+	public function resolveUserLogin($username, $password) {	
+		$this->db->select('password');
+		$this->db->from('users');
+		$this->db->where('username', $username);
+		$hash = $this->db->get()->row('password');
+		return $this->verify_password_hash($password, $hash);
 	}
 
 
