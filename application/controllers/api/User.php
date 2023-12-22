0e60fac1e2dc4e65fb5b4e97f29a468bcab6ade4
Genielson <ge.nielson@hotmail.com>
2023-12-21 22:43:56 -0300

    feat: Implementation of the userRegister method in User controller to signup users

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -9,8 +9,43 @@ class User extends REST_Controller {
 	
 	public function __construct() {
 		parent::__construct();
-        $this->load->library('Authorization_Token');
+      $this->load->library('Authorization_Token');
 		$this->load->model('UserModel');
+	}
+
+
+	public function registerPost() {
+
+	
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|alpha_numeric|min_length[4]|is_unique[users.username]', array('is_unique' => 'This username already exists. Please choose another one.'));
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[6]');
+		
+		if ($this->form_validation->run() === false) {
+            $this->response(['Regras de validação violadas'], REST_Controller::HTTP_OK);
+		} else {
+			
+			$username = $this->input->post('username');
+			$email    = $this->input->post('email');
+			$password = $this->input->post('password');
+			
+			if ($res = $this->user_model->create_user($username, $email, $password)) {
+                $token_data['uid'] = $res; 
+                $token_data['username'] = $username;
+                $tokenData = $this->authorization_token->generateToken($token_data);
+                $final = array();
+                $final['access_token'] = $tokenData;
+                $final['status'] = true;
+                $final['uid'] = $res;
+                $final['message'] = 'Obrigado por registrar sua nova conta!';
+                $this->response($final, REST_Controller::HTTP_OK); 
+
+			} else {
+                $this->response(['Ocorreu um problema ao criar sua nova conta. Por favor, tente novamente.'], REST_Controller::HTTP_OK);
+			}
+			
+		}
+		
 	}
 
 	
