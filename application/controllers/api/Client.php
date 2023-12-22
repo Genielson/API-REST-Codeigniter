7cac1fed54f0d345cdd3e001a69c9fef1f997d67
Genielson <ge.nielson@hotmail.com>
2023-12-21 23:22:46 -0300

    feat: Implementation of the indexGet  method in Client controller for search the id of client

--- application/controllers/api/Client.php
+++ application/controllers/api/Client.php
@@ -15,6 +15,34 @@ class Client extends REST_Controller {
        $this->load->library('Authorization_Token');	
        $this->load->model('ClientModel');
     }
+
+
+    /**
+     * SHOW | GET method.
+     *
+     * @return Response
+    */
+    public function indexGet(int $id = 0)
+    {
+        $headers = $this->input->request_headers(); 
+        if (isset($headers['Authorization'])) {
+            $decodedToken = $this->authorization_token->validateToken($headers['Authorization']);
+            if ($decodedToken['status'])
+            {
+                if(!empty($id)){
+                    $data = $this->ClientModel->show($id);
+                } else {
+                    $data = $this->ClientModel->show();
+                }
+                $this->response($data, REST_Controller::HTTP_OK);
+            } 
+            else {
+                $this->response($decodedToken);
+            }
+        } else {
+            $this->response(['Falha de autenticação.'], REST_Controller::HTTP_OK);
+        }
+    }
        
    	
 }
