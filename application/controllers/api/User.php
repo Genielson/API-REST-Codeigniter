a4a2f063113495e5d38d2afd01e9dfce7c17b67d
Genielson <ge.nielson@hotmail.com>
2023-12-21 22:50:05 -0300

    feat: Implementation of the userLogin method in User controller for signin of the  users

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -14,10 +14,8 @@ class User extends REST_Controller {
 	}
 
 
-	public function registerPost() {
-
-	
-		$this->form_validation->set_rules('username', 'Username', 'trim|required|alpha_numeric|min_length[4]|is_unique[users.username]', array('is_unique' => 'This username already exists. Please choose another one.'));
+	public function registerUser() {
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|alpha_numeric|min_length[4]|is_unique[users.username]', array('is_unique' => 'Esse nome já existe, por favor, escolha outro! '));
 		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|is_unique[users.email]');
 		$this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[6]');
 		
@@ -42,6 +40,49 @@ class User extends REST_Controller {
 
 			} else {
                 $this->response(['Ocorreu um problema ao criar sua nova conta. Por favor, tente novamente.'], REST_Controller::HTTP_OK);
+			}
+			
+		}
+		
+	}
+
+
+	public function loginUser() {
+	
+		$this->form_validation->set_rules('username', 'Username', 'required|alpha_numeric');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+
+		if ($this->form_validation->run() == false) {
+			
+            $this->response(['Regras de validação violadas'], REST_Controller::HTTP_OK);
+
+		} else {
+			
+			$username = $this->input->post('username');
+			$password = $this->input->post('password');
+			
+			if ($this->user_model->resolve_user_login($username, $password)) {
+				
+				$user_id = $this->user_model->get_user_id_from_username($username);
+				$user    = $this->user_model->get_user($user_id);
+				
+				$_SESSION['user_id']      = (int)$user->id;
+				$_SESSION['username']     = (string)$user->username;
+				$_SESSION['logged_in']    = (bool)true;
+				$_SESSION['is_confirmed'] = (bool)$user->is_confirmed;
+				$_SESSION['is_admin']     = (bool)$user->is_admin;
+
+                $token_data['uid'] = $user_id;
+                $token_data['username'] = $user->username; 
+                $tokenData = $this->authorization_token->generateToken($token_data);
+                $final = array();
+                $final['access_token'] = $tokenData;
+                $final['status'] = true;
+                $final['message'] = 'Login realizado com sucesso!';
+                $final['note'] = 'Você está logado';
+                $this->response($final, REST_Controller::HTTP_OK); 	
+			} else {
+                $this->response(['Senha ou login incorretos. '], REST_Controller::HTTP_OK);			
 			}
 			
 		}
